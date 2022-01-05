7bcf8e39bede43bbe189eae6aaaac754b84bee89
Matheus-Augusto-Silva <matheusaugusto-18@hotmail.com>
2022-01-04 23:15:02 -0300

    Alterações de finalização da To-do-List

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,44 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    // Se estiver vazio, não retorna nada
+    if (!newTaskTitle) return; 
+
+    // Se não estiver vazio, posso criar um objeto newTask com as memas características da interface Task
+    const newTask = {
+      id:Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    // Feito isto, adiciono a tarefa em questão ao array de tarefas
+    setTasks(tasks => [...tasks, newTask])
+    
+    // e por fim, atribuo o valor vazio ao setNewTaskTitle para "zerar" o título da tarefa
+    setNewTaskTitle("")
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    //Vou filtrar todos os id´s q são diferentes do ID da task para obter apenas ele.
+    let filteredTasks = tasks.filter(task => task.id !== id)
+
+    // e então salvarei as tarefas em qustão ao estado
+    setTasks(filteredTasks)
   }
 
   return (
